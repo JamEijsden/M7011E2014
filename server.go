f2e4888dae3b505e781b137617664c49cd5ebc6c
Trolle <trolle.geuna@gmail.com>
2014-11-12 15:04:38 +0100

    Added some things fore fb support

--- server.go
+++ server.go
@@ -61,7 +61,7 @@ func main() {
 	http.HandleFunc("/about/", aboutHandler)
 	http.HandleFunc("/test/", testHandler)
 
-	http.ListenAndServe("localhost:80", nil) // listen for connections at port 9999 on the local machine
+	http.ListenAndServe("localhost:9999", nil) // listen for connections at port 9999 on the local machine
 }
 
 // HIDE DICRECTORY
