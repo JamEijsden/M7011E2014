8d9daec819aeda64c80d2a659959bdc86422d29c
jameijsden <jameijsden@gmail.com>
2014-11-17 14:30:30 -0800

    added map to test

--- server.go
+++ server.go
@@ -62,14 +62,14 @@ func main() {
 	http.HandleFunc("/test/", testHandler)
 
 	var i int
-	fmt.Println("Run server on:\n1. localhost:9999\n192.168.1.230:9999\nChoose a connection(1 or 2).")
+	fmt.Println("Run server on:\n1. localhost:9999\n2. 192.168.1.230:9999\nChoose a connection(1 or 2).")
 	fmt.Scan(&i)
 	if i == 1 {
 		fmt.Println("Server running on localhost:9999")
 		http.ListenAndServe("localhost:9999", nil)
 	} else if i == 2 {
 		fmt.Println("Server running on 192.168.1.230:9999")
-		http.ListenAndServe("192.168.1.230:9999", nil) // listen for connections at port 9999 on the local machine
+		http.ListenAndServe("192.168.1.230:9999", nil)
 	} else {
 		fmt.Println("Not a valid option")
 	}
