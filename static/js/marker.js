919c6dfbd864110d80b47d5fd66a9b10ddba8732
jameijsden <jameijsden@gmail.com>
2014-11-18 22:19:41 -0800

    working marker

--- static/js/marker.js
+++ static/js/marker.js
@@ -1,4 +1,13 @@
 var markerPlaced = 0
+
+
+
+function updateDrag(location) {
+  var input = $('#loc');
+  input.val(location);
+
+}
+
 function placeMarker(location) {
   if(markerPlaced == 0){
     var marker = new google.maps.Marker({
@@ -6,10 +15,17 @@ function placeMarker(location) {
                 map: map,
                 draggable:true,
                 animation: google.maps.Animation.DROP,
-                title:"Stair!",
+                title:"HEJ",
               icon: "http://maps.google.com/mapfiles/ms/micons/blue.png"
             });
+    var input = $('#loc');
+    input.val(location);
   }
     
     markerPlaced = 1
+    google.maps.event.addListener(marker, 'dragend', function(event){
+    updateDrag(marker.getPosition())
+    });
+    
+    
 }
