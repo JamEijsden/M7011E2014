741559c9d03cf471b68ed87a9367b44bf3dda0a0
Trolle <trolle.geuna@gmail.com>
2014-11-24 15:53:00 +0100

    more stuff

--- api_server/mainfromutube.go
+++ api_server/mainfromutube.go
@@ -46,15 +46,52 @@ func getAllUsers(w http.ResponseWriter, r *http.Request) {
 func getUser(rw http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	id := vars["key"]
+
+	/*
+		retrive the user from the db
+		and put in a user struct
+	*/
+	var usr User
+	//add the other lines
+	// then
+	ans := usr.jsonResponseUser()
+	fmt.Printf(rw, string(ans))
+
 }
 
 func addUser(rw http.ResponseWriter, req *http.Request) {
-	//usr, err := fixParsingRequest(rw, req)
+	// read body
+	data, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		http.Error(rw, err.Error(), 500)
+		return
+	}
+	var usr User
+	err = json.Unmarshal(data, &usr)
+	if err != nil {
+		http.Error(rw, err.Error(), 500)
+	}
+
 	//vars := mux.Vars(req)
 	//id := vars["key"]
 	//d := user
 	//d.userID = id
 
+	/*
+		here we want to add this to the database
+		using usr ass the parameters
+	*/
+
+}
+
+func (u *User) jsonResponseUser() []byte {
+	jsonencoded, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println("error int jsonAnswerUser")
+		return nil
+	}
+	return jsonencoded
 }
 
 func fixParsingRequest(rw http.ResponseWriter, req *http.Request) {
@@ -68,11 +105,11 @@ func fixParsingRequest(rw http.ResponseWriter, req *http.Request) {
 
 	// Unmarshalling the req param
 	var usr User
-	err = json.Unmarshal(b, &usr)
+	err = json.Unmarshal(data, &usr)
 	if err != nil {
 		http.Error(rw, err.Error(), 500)
 		return
 	}
-	return usr, nil
+	return usr
 
 }
